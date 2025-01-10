387677a4c9b283a52d7d7ada3d7eae61dbec7253
Maria Pyatibrat <masapyatibrat@gmail.com>
2025-01-10 12:27:18 +0300

    - Функция: реализовать создание и удаление карточек v1

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,26 +1,26 @@
 const initialCards = [
     {
-      name: "Архыз",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+      name: "Красная поляна",
+      link: "https://images.unsplash.com/photo-1603787292746-92adce40cbef?q=80&w=2835&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
-      name: "Челябинская область",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+      name: "Ягоды",
+      link: "https://images.unsplash.com/photo-1668845008156-57e69d1d1b9e?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fCVEMCVCRiVEMSU4MCVEMCVCOCVEMSU4MCVEMCVCRSVEMCVCNCVEMCVCMHxlbnwwfHwwfHx8MA%3D%3D",
     },
     {
-      name: "Иваново",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+      name: "Розы",
+      link: "https://images.unsplash.com/photo-1681545808623-3c1c6ce1ffed?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8ODN8fCVEMCVCRiVEMSU4MCVEMCVCOCVEMSU4MCVEMCVCRSVEMCVCNCVEMCVCMHxlbnwwfHwwfHx8MA%3D%3D",
     },
     {
-      name: "Камчатка",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+      name: "Домашний кот",
+      link: "https://images.unsplash.com/photo-1706942874373-378d6cdc3ef7?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
-      name: "Холмогорский район",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+      name: "Снежный пейзаж",
+      link: "https://plus.unsplash.com/premium_photo-1670260849910-cb8c8df8191d?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
-      name: "Байкал",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+      name: "Пустыня",
+      link: "https://images.unsplash.com/photo-1547235001-d703406d3f17?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fCVEMCVCRiVEMCVCNSVEMCVCOSVEMCVCNyVEMCVCMCVEMCVCNnxlbnwwfHwwfHx8MA%3D%3D",
     }
 ];
--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,44 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+
+// Контейнер для карточек
+const placesList = document.querySelector('.places__list');
+
+// Шаблон карточки
+const cardTemplate = document.querySelector('#card-template').content;
+
+// Функция создания карточки
+function createCard(data, onDelete) {
+
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+
+    const cardImage = cardElement.querySelector('.card__image');
+    const cardTitle = cardElement.querySelector('.card__title');
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+
+    cardImage.src = data.link;
+    cardImage.alt = data.name;
+    cardTitle.textContent = data.name;
+
+    deleteButton.addEventListener('click', () => onDelete(cardElement));
+
+    return cardElement;
+}
+
+// Функция удаления карточки
+function handleDeleteCard(cardElement) {
+    cardElement.remove();
+}
+
+// Функция вывода всех карточек на страницу
+function renderCards(cards) {
+    cards.forEach(card => {
+        const cardElement = createCard(card, handleDeleteCard);
+        placesList.appendChild(cardElement);
+    });
+}
+
+// Выводим карточки из массива на страницу
+renderCards(initialCards);
